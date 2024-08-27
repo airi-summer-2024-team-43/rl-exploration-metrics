8be7f95f3bdc10c7aed7d7712900cad3fba257f3
Nikita <nikitazag3@gmail.com>
2024-08-27 20:03:02 +0000

    metric counter (by N) update

--- metric_state_counting.py
+++ metric_state_counting.py
@@ -9,15 +9,23 @@ def args_for_state_counting(algo_args):
     return ArgsStateCounter
 
 
-class StateCounter:
+class StateCounter():
     def __init__(self):
         pass
 
-    def get_cell(obs):
-        x, y, *_ = obs["observation"]
-        shifted_x = round(x + 4.5)
-        shifted_y = round(y + 3.0)
+    def get_cell(self, obs):
+        x, y, *_ = obs
+        shifted_x = int(x.item() + 5.0)
+        shifted_y = int(y.item() + 3.5)
 
         return shifted_x + shifted_y * 10
+    
+    def update_visited_states(self, obs, visited_states):
+        rewards = [0] * len(visited_states)
+        for i in range(len(visited_states)):
+            if self.get_cell(obs[i]) not in visited_states[i]:
+                rewards[i] += 1
+            visited_states[i].add(self.get_cell(obs[i]))
 
+        return rewards, visited_states
     
