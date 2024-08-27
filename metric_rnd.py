9cb6cc27322e0d284c666b13de59e394f5f611be
Nikita <nikitazag3@gmail.com>
2024-08-27 20:03:42 +0000

    metric RND update (by N)

--- metric_rnd.py
+++ metric_rnd.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 
 def args_for_rnd(algo_args):
@@ -11,7 +12,7 @@ def args_for_rnd(algo_args):
         # RND
         ext_coef: float = 1.0
         """RND extrinsic reward coef"""
-        int_coef: float = 0.5
+        int_coef: float = 0.01
         """RND intrinsic reward coef"""
         output_rnd: int = 32
         """last layer RND output size"""
@@ -55,23 +56,30 @@ class RNDModel(nn.Module):
         for param in self.target.parameters():
             param.requires_grad = False
 
+        self.optimizer = optim.Adam(self.parameters(), lr=1e-5)
+
     def forward(self, obs):
         target_feature = self.target(obs)
         predict_feature = self.predictor(obs)
 
         return predict_feature, target_feature
 
+    @torch.no_grad()
+    def get_intrinsic_reward(self, obs):
+        predict_feature, target_feature = self.forward(obs)
+        return 0.5 * (predict_feature - target_feature).pow(2).mean(-1)
+
     def get_forward_loss(self, obs, mask_proportion=0.25):
         predict_feature, target_feature = self.forward(obs)
         loss = F.mse_loss(predict_feature, target_feature.detach(), reduction="none")
-        mask = torch.rand(len(loss), device=loss.device)
+        mask = torch.rand_like(loss, device=loss.device)
         mask = (mask < mask_proportion).type(torch.FloatTensor).to(loss.device)
         loss = (loss * mask).sum() / torch.max(
             mask.sum(), torch.tensor([1], device=loss.device, dtype=torch.float32)
         )
         return loss
 
-    @torch.no_grad()
-    def get_intrinsic_reward(self, obs):
-        predict_feature, target_feature = self.forward(obs)
-        return 0.5 * (predict_feature - target_feature).pow(2).sum(-1)
+    def update(self, loss):
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
