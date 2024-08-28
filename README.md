81f362362858b79b48015676549318029c347f53
Nikita <nikitazag3@gmail.com>
2024-08-28 12:13:25 +0000

    mini update

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ conda activate ./.conda
 cd project
 python ppo_experiments.py
 ```
+
+```shell
+python ppo_experiments.py --use_rnd_metric --use_state_counting_metric --track
+```
--- test_metrics.py
+++ test_metrics.py
@@ -1,6 +1,9 @@
 def test_args():
     from ppo_experiments import Args
-    from metric_rnd import args_for_RND
+    from metric_rnd import args_for_rnd
 
-    c = args_for_RND(Args)()
+    c = args_for_rnd(Args)()
     print(c.wandb_project_name)
+
+
+def test_metric_on_random_maze(): ...
