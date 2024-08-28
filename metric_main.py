285bdfd158673d6d3fb3745f9c3f19e8f564a580
Nikita <nikitazag3@gmail.com>
2024-08-28 18:05:53 +0000

    add disagreement metric to shared interface

--- metric_main.py
+++ metric_main.py
@@ -3,30 +3,32 @@ import torch
 
 from metric_rnd import RNDModel, args_for_rnd
 from metric_state_counting import StateCounter, args_for_state_counting
+from metric_disagreement import Ensemble, args_for_disagreement
 
 
 class MetricsUsage:
     def __init__(self) -> None:
         self.args = None
-        self.metrics = list()  # rnd, count
+        self.metrics = list()  # rnd, count, model_disagreement
 
-    def update_arg_class(self, Old_Args):
-        # TODO: как это сделать нормально, ибо вариант с if плохо работает
-        New_Args = Old_Args
-        New_Args = args_for_rnd(New_Args)
-        New_Args = args_for_state_counting(New_Args)
-        return New_Args
+    def update_arg_class(self, args):
+        args_class = args
+        args_class = args_for_rnd(args_class)
+        args_class = args_for_state_counting(args_class)
+        args_class = args_for_disagreement(args_class)
+        return args_class
 
     def _init_metrics_running_info(self):
         self.running_int_rewards = []
         self.running_state_counts = []
+        self.episode_disagreements = []
+        self.running_disagreements = []
         self.state_counts = [set() for _ in range(self.args.num_envs)]
         self.int_rewards = torch.zeros((self.args.num_steps, self.args.num_envs)).to(
             self.device
         )
 
     def _update_args(self, args):
-        # TODO: не делать отдельный флаг метрики, а их перечислить?
         self.args = args
 
         args.use_rnd_metric = args.use_rnd_metric or args.use_rnd_intrinsic_reward
@@ -34,6 +36,8 @@ class MetricsUsage:
             self.metrics.append("rnd")
         if args.use_state_counting_metric:
             self.metrics.append("count")
+        if args.use_model_disagreement_metric:
+            self.metrics.append("model_disagreement")
 
     def init_metrics_info(self, args, obs_space, writer, device):
         self.writer = writer
@@ -53,7 +57,19 @@ class MetricsUsage:
             else:
                 raise NotImplementedError("Unknown RND model type")
         if self.args.use_state_counting_metric:
-            self.state_counter = StateCounter()
+            map_name = args.env_map
+            if map_name is None:
+                self.state_counter = StateCounter()
+            else:
+                from maze_maps import maps
+
+                map_size = np.array(maps[map_name]).shape
+                self.state_counter = StateCounter(
+                    x_size=map_size[1], y_size=map_size[0]
+                )
+
+        if self.args.use_model_disagreement_metric:
+            self.ensemble = Ensemble(args)
 
     def update_intrinsic_reward(self, obs, rewards):
         args = self.args
@@ -69,10 +85,20 @@ class MetricsUsage:
                 )
         return rewards
 
+    def update_disagreement(self, obs, actions):
+        if self.args.use_model_disagreement_metric:
+            disagreement = self.ensemble.get_disagreement(obs, actions)
+            self.episode_disagreements.append(disagreement)
+
     def update_learnable_metric(self, obs):
         if self.args.use_rnd_metric:
             exploration_loss = self.rnd_model.get_forward_loss(obs)
             self.rnd_model.update(exploration_loss)
+
+    def update_with_action(self, obs, action, next_obs):
+        if self.args.use_model_disagreement_metric:
+            disagreement_loss = self.ensemble.get_ensemble_loss(obs, action, next_obs)
+            self.ensemble.update(disagreement_loss)
 
     def update_with_state(self, next_obs):
         if self.args.use_state_counting_metric:
@@ -81,7 +107,7 @@ class MetricsUsage:
             )
             state_count_rewards = np.mean(state_count_rewards)
 
-    def end_episode_update(self, episodic_length):
+    def end_episode_update(self):
         if self.args.use_rnd_metric:
             self.running_int_rewards.append(
                 self.int_rewards[-self.args.num_steps :].cpu().numpy().mean()
@@ -92,13 +118,21 @@ class MetricsUsage:
                 / self.state_counter.num_states
             )
             self.running_state_counts.append(metric)
+        if self.args.use_model_disagreement_metric:
+            metric = np.mean(self.episode_disagreements)
+            self.episode_disagreements = []
+            self.running_disagreements.append(metric)
 
-    def log_metrics(self, iteration):
+    def log_metrics(self, step):
         if self.args.use_rnd_metric:
             self.writer.add_scalar(
-                "metric/novelty_rnd", self.running_int_rewards[-1], iteration
+                "metric/novelty_rnd", self.running_int_rewards[-1], step
             )
         if self.args.use_state_counting_metric:
             self.writer.add_scalar(
-                "metric/state_counts", self.running_state_counts[-1], iteration
+                "metric/state_counts", self.running_state_counts[-1], step
+            )
+        if self.args.use_model_disagreement_metric:
+            self.writer.add_scalar(
+                "metric/model_disagreement", self.running_disagreements[-1], step
             )
